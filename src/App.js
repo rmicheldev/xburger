675cdc046e88dd57b56c2ffa3a6e93e9b035c093
rmichel <rodrigomichel@outlook.com>
2021-11-07 17:25:48 -0300

    Primeira pagina funcional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import api from "./services/api";
+import React, { useEffect, useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [userData, setUserData] = useState([]);
+    useEffect(() => {
+      api
+        .get("/api/v1/restaurants?page=1&limit=10")
+        .then((response) => {
+          console.log("Dados", response.data.data);
+          console.log("Paginação", response.data.pagination);
+          setUserData(response.data.data);
+        })
+        .catch((err) => {
+          console.error("ops! ocorreu um erro" + err);
+        });
+    }, []);
+
+    
+    let itemList = userData.map((item,index)=>{
+      return <div key={index}>
+        <div class="col">
+          <div class="p-3 border bg-light">
+              <img src={item.image} class="img-fluid"/>
+              <div class="text-inf-dir">{item.name}</div>
+          </div>
+        </div>
+      </div>
+    })
+
+    return (
+      <div>
+        <div class="header-fw">
+          <img src="https://cdn.pixabay.com/photo/2018/05/13/20/21/lake-3397784_960_720.jpg" class="img-fw"/>
+        </div>
+        <div class="container p-3 ">
+          <div class="row row-cols-1 row-cols-lg-2 row-cols-md-1 row-cols-sm-1 row-cols-xs-1 g-2 g-lg-1">
+            {itemList}
+          </div>
+        </div>
+      </div>
+    );
 }
 
-export default App;
+export default App
