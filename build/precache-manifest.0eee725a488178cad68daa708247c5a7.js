6c31679c3951d812d718148441628e3a9665ffd7
rmichel <rodrigomichel@outlook.com>
2021-11-08 03:31:36 -0300

    adicionando build

---
+++ build/precache-manifest.0eee725a488178cad68daa708247c5a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78bd829ef9c372d32701ef95076c7aec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e38bc478b411b9a67837",
+    "url": "/static/css/2.e7235f0e.chunk.css"
+  },
+  {
+    "revision": "8227c394dd732c43483c",
+    "url": "/static/css/main.dc0db645.chunk.css"
+  },
+  {
+    "revision": "e38bc478b411b9a67837",
+    "url": "/static/js/2.e5c5397e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e5c5397e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8227c394dd732c43483c",
+    "url": "/static/js/main.481e2f65.chunk.js"
+  },
+  {
+    "revision": "9e1a83853227263e42a6",
+    "url": "/static/js/runtime-main.7aa27b02.js"
+  },
+  {
+    "revision": "f4e0e157cd7948c574718b1ed0539db8",
+    "url": "/static/media/logo.f4e0e157.svg"
+  }
+]);
